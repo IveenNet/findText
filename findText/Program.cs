f1ce987d4f3d95b601a4444cc9759d104881cf92
Ivan David Medina Vallez <iveen98@gmail.com>
2025-10-31 13:21:53 +0100

    Primeros Cambios

--- findText/Program.cs
+++ findText/Program.cs
@@ -1,4 +1,5 @@
 using findText;
+using findText.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +8,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton<FileContentService>();
 
 await builder.Build().RunAsync();
---
+++ findText/Services/FileContentService.cs
@@ -0,0 +1,29 @@
+﻿namespace findText.Services
+{
+    // FileContentService.cs
+    public class FileContentService
+    {
+        public string File1Content { get; set; } = string.Empty;
+        public string File2Content { get; set; } = string.Empty;
+
+        public event Action OnChange;
+
+        public void SetFileContent(int fileNumber, string content)
+        {
+            if (fileNumber == 1)
+                File1Content = content;
+            else if (fileNumber == 2)
+                File2Content = content;
+
+            NotifyStateChanged();
+        }
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+
+        public string GetCombinedContent()
+        {
+            // Esta es la lógica para el textarea central
+            return $"--- Contenido Archivo 1 ---\n{File1Content}\n\n--- Contenido Archivo 2 ---\n{File2Content}";
+        }
+    }
+}
